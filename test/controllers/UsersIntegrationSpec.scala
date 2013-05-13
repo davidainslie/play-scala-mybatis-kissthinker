49f26517c4313e1e106b63f74adc210b9f60461d
David Ainslie <dainslie@gmail.com>
2013-05-13 01:19:43 +0100

    Chrome driver introduced.

--- test/controllers/UsersIntegrationSpec.scala
+++ test/controllers/UsersIntegrationSpec.scala
@@ -3,15 +3,22 @@ package controllers
 import org.specs2.mutable.Specification
 import play.api.test.Helpers._
 import play.api.http.HeaderNames
-import play.api.test.{WithBrowser, FakeRequest}
+import play.api.test._
 import models.User
+import play.api.test.TestServer
+import play.api.test.FakeApplication
+import org.openqa.selenium.firefox.{FirefoxDriver, FirefoxProfile}
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
 
 class UsersIntegrationSpec extends Specification {
+  System.setProperty("webdriver.chrome.driver", "C:/git/play-scala-mybatis-kissthinker/chromedriver.exe")
+
   "User" should {
-    "view a user profile" in new WithBrowser {
-      browser.goTo("/users/1")
-      //browser.title() mustEqual "User"
-      pending
+    "view a user profile" in new WithBrowser(webDriver = classOf[ChromeDriver]) {
+      browser.goTo("/")
+      browser.click("#user1")
+      browser.find("#content") contains "User ID: 1"
     }
   }
 }
